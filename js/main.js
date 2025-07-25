b3a283be663bb249b5701b5d270a07f354eb4124
Purnima-Pal <119881467+Purnima-Pal@users.noreply.github.com>
2025-07-25 09:27:55 +0530

    popup1 and pop2 done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // script for close btn
 let popup1CloseBtn = document.getElementById("popup1_close_btn");
-let getpopup1 = document.querySelectorAll(".popup1");
+let getPopup1 = document.getElementById("popup1");
 popup1CloseBtn.addEventListener("click",function(){
-   getpopup1[0].style.display="none";
+   getPopup1.style.display="none";
 })
 
 // script for email icon
@@ -26,3 +26,50 @@ getLogwithMobbtn.addEventListener("click",function(){
    getEmailIcon.style.display ="block";
    getLogwithMobbtn.style.display = "none"; 
 })
+
+
+// script for popup Tab Button
+
+let getPopUpBtn1 = document.querySelectorAll(".popup1_tab_btn button");
+let getPopUp1content = document.querySelectorAll(".popup_tab_content");
+let getPopUp1Img = document.querySelectorAll(".popup_img_wrap img");
+getPopUp1content.forEach((conetctItemshow, index)=>{conetctItemshow.style.display = index === 0 ? "block":"none"});
+getPopUp1Img.forEach((imgShow, index) =>{imgShow.style.display = index === 0?"block":"none"});
+    getPopUpBtn1.forEach((btnItemAdd,index) =>{
+      btnItemAdd.addEventListener("click",function(){
+      getPopUpBtn1.forEach(btnItemRemove => btnItemRemove.classList.remove("active"));
+      btnItemAdd.classList.add("active");
+      getPopUp1content.forEach(conetctItemRemove =>conetctItemRemove.style.display ="none");
+      getPopUp1content[index].style.display ="block";
+      getPopUp1Img.forEach((imgItemRemove)=>imgItemRemove.style.display ="none");
+      getPopUp1Img[index].style.display ="block";
+      if(btnItemAdd.classList.contains("active") && index === 1){
+       popup1CloseBtn.style.display = "none" ; 
+      } else{
+         popup1CloseBtn.style.display = "block" ; 
+      }
+   })
+    })
+
+
+          // script for forgot_btn
+   let getForgotBtn = document.getElementById("forgot_btn");
+   let getPopUp2 = document.getElementById("popup2");
+   getPopUp2.style.display="none";
+   getForgotBtn.addEventListener("click", function(){
+   getPopup1.style.display="none";
+   getPopUp2.style.display="flex";
+   })
+
+   // script for backbtn
+   let getBackbtn = document.getElementById("popup2_backbtn");
+   getBackbtn.addEventListener("click",function(){
+   getPopup1.style.display="flex";
+   getPopUp2.style.display="none";
+   })
+
+
+ 
+
+
+    
